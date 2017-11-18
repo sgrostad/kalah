9924cbd94a60ceef0a5dbb92f26d304c8353407d
sindre <sindre.grostad@icloud.com>
2017-11-18 12:34:06 +0000

    first commit

---
+++ src/Side.java
@@ -0,0 +1,20 @@
+/**
+ * The side of the Kalah board a player can choose.
+ */
+public enum Side
+{
+	NORTH, SOUTH;
+
+	/**
+	 * @return the side opposite to this one. 
+	 */
+	public Side opposite()
+	{
+		switch (this)
+		{
+			case NORTH: return SOUTH;
+			case SOUTH: return NORTH;
+			default: return NORTH;  // dummy
+		}
+	}
+}
