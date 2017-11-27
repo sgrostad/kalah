a252c6607b2f080e0f579a2bff97e33ad7992684
sindre <sindre.grostad@icloud.com>
2017-11-27 20:35:21 +0000

    Moved functions that recognize special movesto new class. Added game won/lost in heuristic

--- src/Heuristic.java
+++ src/Heuristic.java
@@ -5,7 +5,6 @@ public class Heuristic {
     private static double stonesInStoreCoefficient = 2;
     private static double stonesInHolesCoefficient = 0.5;
     private static double freeTurnMovesCoefficient = 0.5;
-    private static double stealSeedMovesCoefficient = 0.5;
     private static double maxStealSeedMoveCoefficient = 0.5;
 
     // Every function ending with "Diff" are used to make heuristics. Actual heuristic in the bottom
@@ -39,35 +38,11 @@ public class Heuristic {
     private static int countFreeMoves(Board board, Side side){
         int count = 0;
         for(int hole = 1; hole <= board.getNoOfHoles(); hole++){
-            if(isFreeTurnMove(board, side, hole)){
+            if(MoveClassifier.isFreeTurnMove(board, side, hole)){
                 count++;
             }
         }
         return count;
-    }
-
-    private static boolean isFreeTurnMove(Board board, Side side, int hole){
-        int stonesInHole = board.getSeeds(side, hole);
-        int noOfHoles = board.getNoOfHoles();
-        if(stonesInHole % (noOfHoles*2 + 1) == noHolesToStore(board, side, hole)){
-            return true;
-        }
-        return false;
-    }
-
-    private static int noHolesToStore(Board board, Side side, int hole){
-        if (side == Side.NORTH){
-            return hole;
-        }
-        else{
-            return board.getNoOfHoles() - hole + 1;
-        }
-    }
-    /* Do not think this is a good heuristic:*/
-    private static int noStealMovesDiff(Board board, Side side){
-        int ownStealMoves = countStealMoves(board, side);
-        int oppStealMoves = countStealMoves(board, side.opposite());
-        return ownStealMoves - oppStealMoves;
     }
 
     private static int maxStealMoveDiff(Board board, Side side){
@@ -79,8 +54,8 @@ public class Heuristic {
     private static int maxStealMove(Board board, Side side){
         int maxSteal = 0;
         for(int hole = 1; hole <= board.getNoOfHoles(); hole++){
-            if(isStealSeedsMove(board, side, hole)){
-                int endHole = findEndHole(board, side, hole);
+            if(MoveClassifier.isStealSeedsMove(board, side, hole)){
+                int endHole = MoveClassifier.findEndHole(board, side, hole);
                 int stealValue = board.getSeeds(side.opposite(), endHole);
                 if (stealValue > maxSteal){
                     maxSteal = stealValue;
@@ -90,58 +65,9 @@ public class Heuristic {
         return maxSteal;
     }
 
-    private static int countStealMoves(Board board, Side side){
-        int count = 0;
-        for(int hole = 1; hole <= board.getNoOfHoles(); hole++){
-            if(isStealSeedsMove(board, side, hole)){
-                count++;
-            }
-        }
-        return count;
-    }
-
-    private static boolean isStealSeedsMove(Board board, Side side, int hole){
-        int stealFromHole = findEndHole(board, side, hole);
-        if (!enoughSeedsForOverFullRound(board, side, hole)
-                && board.getSeeds(side, hole) > 0
-                && stealFromHole > 0
-                && board.getSeedsOp(side, stealFromHole) > 0){
-            return true;
-        }
-        return false;
-    }
-
-    private static boolean enoughSeedsForOverFullRound(Board board, Side side, int hole){
-        int seeds = board.getSeeds(side, hole);
-        int noHoles = board.getNoOfHoles();
-        if(seeds > noHoles * 2 + 1){
-            return true;
-        }
-        return false;
-    }
-
-    private static int findEndHole(Board board, Side side, int hole){
-        int seeds = board.getSeeds(side, hole);
-        int distanceToStore = noHolesToStore(board, side, hole);
-        int noHoles = board.getNoOfHoles();
-        seeds = seeds % (noHoles * 2 + 1);
-        if(side == Side.NORTH){
-            if(seeds < distanceToStore){
-                return hole - seeds;
-            }
-            else if(seeds > distanceToStore + noHoles){ // to many seeds already handled
-                return noHoles - (seeds - distanceToStore - noHoles);
-            }
-        }
-        else{ //Side.South
-            if (seeds < distanceToStore){
-                return hole + seeds;
-            }
-            else if(seeds > distanceToStore + noHoles){// to many seeds already handled
-                return seeds - distanceToStore - noHoles;
-            }
-        }
-        return 0;
+    //TODO implement function that return coefficient based on how far we have reached in the game.
+    private static double getStonesInStoreCoefficient(){
+        return stonesInStoreCoefficient;
     }
 
     public static double simpleHeuristic(Board board, Side side){
@@ -149,11 +75,17 @@ public class Heuristic {
                 + stonesInHolesCoefficient * stonesInHolesDiff(board, side);
     }
 
+    //TODO confirm that Double.POSITIVE_INFINITY works with double as return variable
     public static double advancedHeuristic(Board board, Side side){
+        if (Kalah.gameOver(board)){
+            if(board.getSeedsInStore(side) > board.getSeedsInStore(side.opposite())){
+                return Double.POSITIVE_INFINITY;
+            }
+            return Double.NEGATIVE_INFINITY;
+        }
         return stonesInStoreCoefficient * stonesInStoreDiff(board, side)
                 + stonesInHolesCoefficient * stonesInHolesDiff(board, side)
                 + freeTurnMovesCoefficient * freeTurnMovesDiff(board, side)
-                + stealSeedMovesCoefficient * noStealMovesDiff(board, side) /* Do not think this is a good heuristic*/
                 + maxStealSeedMoveCoefficient * maxStealMoveDiff(board, side);
     }
 }
