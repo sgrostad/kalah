1120be19520ace85228083302280da0e0ea3208b
sindre <sindre.grostad@icloud.com>
2017-11-29 17:05:55 +0000

    A first draft of possible solution to do the search. NOT checked if works as intended

--- src/BoardNode.java
+++ src/BoardNode.java
@@ -1,107 +1,96 @@
 public class BoardNode {
-    private BoardNode parentBoardNode;
+
+    private BoardNode parentBoardNode; //TODO for the time being not used. Remove if not needed.
     private Board currentBoard;
-    private BoardNode[] childBoardNodes;
+
+    private Move parentMove;
+    private Move minMaxMove;
+
+    private static Side maximizingPlayer; //TODO look for better solution?
 
     private Side nextPlayerTurn;
-    private Move parentMove;
+    private int depth; //TODO Agree how we define depth. Now it is decreasing when changing turn.
+
     private boolean swapAvailable;
 
-    private double currentHeuristic;
-    private static final int terminalDepth = 7; //can be chosen
-    private int depth; //TODO agree when to increase this. now: only when player turn swaps
-
-    private double alpha;
-    private double beta;
-
-    public BoardNode(Board currentBoard, boolean swapAvailable, Side nextPlayerTurn){
+    public BoardNode(Board currentBoard, boolean swapAvailable, Side maximizingPlayer, int searchDepth){
         this.parentBoardNode = null;
         this.currentBoard = currentBoard;
 
-        this.nextPlayerTurn = nextPlayerTurn;
-        parentMove = null;
+        this.maximizingPlayer = maximizingPlayer;
+        this.nextPlayerTurn = maximizingPlayer;
+
+        this.depth = searchDepth;
         this.swapAvailable = swapAvailable;
-
-        setHeuristic();
-        this.depth = 0;
-
-        alpha = Double.NEGATIVE_INFINITY;
-        beta = Double.POSITIVE_INFINITY;
     }
 
-    public BoardNode(BoardNode parentBoardNode, Move move, int depth){
+    public BoardNode(BoardNode parentBoardNode, Move parentMove, int depth){
         this.parentBoardNode = parentBoardNode;
         currentBoard = new Board(parentBoardNode.getCurrentBoard());
 
+        this.parentMove = parentMove;
+
         nextPlayerTurn = Kalah.makeMove(currentBoard, parentMove); // TODO need a similar function that does not report.
-        this.parentMove = move;
-        this.swapAvailable = false;
 
-        setHeuristic();
         this.depth = depth;
-
-        this.alpha = parentBoardNode.getAlpha();
-        this.beta = parentBoardNode.getBeta();
-        if (depth < terminalDepth) {
-            findChildren();
-            sortChildren();
-        }
-        else {
-            childBoardNodes = null;
-        }
+        this.swapAvailable = false;
     }
 
-    private void findChildren(){
-        childBoardNodes = new BoardNode[currentBoard.getNoOfHoles() + 1];
+    public BoardNode[] findChildren(){
+        BoardNode[] childBoardNodes = new BoardNode[currentBoard.getNoOfHoles() + 1];
         int childNo = 0;
         for(int hole = 1; hole <= currentBoard.getNoOfHoles(); hole++){
-            Move childMove = new Move(nextPlayerTurn, hole);
-            if(Kalah.isLegalMove(currentBoard, childMove)){
-                if(parentMove.getSide() == nextPlayerTurn){
-                    //childBoardNodes[childNo] = new BoardNode(this, childMove, false, depth);
-
+            Move move = new Move(nextPlayerTurn, hole);
+            if(Kalah.isLegalMove(currentBoard, move)){
+                if(true/*MoveClassifier.isFreeTurnMove(board, move)*/){
+                    childBoardNodes[childNo] = new BoardNode(this, move, depth);
                 }
                 else{
-                    //childBoardNodes[childNo] = new BoardNode(this, childMove, false, depth + 1);
+                childBoardNodes[childNo] = new BoardNode(this, move, depth - 1);
                 }
                 childNo++;
             }
         }
         if(swapAvailable){
-            //TODO find out how to handle this.
+        //TODO find out how to handle this.
         }
         for(; childNo <= childBoardNodes.length; childNo++){ // Is this necessary?
-            childBoardNodes[childNo] = null;
+        childBoardNodes[childNo] = null;
         }
+        sortChildren();
+        return childBoardNodes;
     }
 
     private void sortChildren(){
         //TODO sort children based on currentHeuristic by some simple insertion sort or something.
+        //double currentHeuristic = Heuristic.advancedHeurisitic(currentBoard, nextPlayerTurn.getSide()); With this heuristic, always sort decreasing
     }
 
+    public static Side getMaximizingPlayer() {
+        return maximizingPlayer;
+    }
 
-    private void setHeuristic(){
-        //currentHeuristic = Heuristic.advancedHeurisitic(currentBoard, parentMove.getSide());
-        currentHeuristic = 0;
+    public Side getNextPlayerTurn() {
+        return nextPlayerTurn;
     }
 
     public Board getCurrentBoard() {
         return currentBoard;
     }
 
-    public double getAlpha() {
-        return alpha;
+    public int getDepth() {
+        return depth;
     }
 
-    public void setAlpha(double alpha) {
-        this.alpha = alpha;
+    public Move getParentMove() {
+        return parentMove;
     }
 
-    public double getBeta() {
-        return beta;
+    public Move getMinMaxMove() {
+        return minMaxMove;
     }
 
-    public void setBeta(double beta) {
-        this.beta = beta;
+    public void setMinMaxMove(Move minMaxMove) {
+        this.minMaxMove = minMaxMove;
     }
 }
--- src/MoveDecisionMaker.java
+++ src/MoveDecisionMaker.java
@@ -1,12 +1,51 @@
 public class MoveDecisionMaker {
+    private static final int searchDepth = 7;
 
     //TODO Convert to iterative instead of recursive for speed
-    public void decideMove(Board board, boolean swapAvailable, Side maxSide){
-        BoardNode rootNode = new BoardNode(board, swapAvailable , maxSide);
-        MinMaxAlphaBeta(rootNode, 0, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+    public static Move decideMove(Board board, boolean swapAvailable, Side maximizingSide){
+        BoardNode rootNode = new BoardNode(board, swapAvailable , maximizingSide, searchDepth);
+        MinMaxAlphaBeta(rootNode, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+        return rootNode.getMinMaxMove();
     }
 
-    private double MinMaxAlphaBeta(BoardNode boardNode, int depth, double alpha, double beta){
+    private static double MinMaxAlphaBeta(BoardNode boardNode, double alpha, double beta){
+        if (boardNode.getDepth() == 0 || Kalah.gameOver(boardNode.getCurrentBoard())){
+            // return Heuristic.advancedHeuristic(boardNode.getCurrentBoard(), boardNode.getMaximizingPlayer());
+        }
+        if (boardNode.getNextPlayerTurn() == BoardNode.getMaximizingPlayer()){
+            double tempAlpha = Double.NEGATIVE_INFINITY;
+            for(BoardNode child : boardNode.findChildren()){
+                if(child == null){
+                    break;
+                }
+                tempAlpha = Math.max(tempAlpha, MinMaxAlphaBeta(child, alpha, beta) );
+                if (tempAlpha > alpha){
+                    alpha = tempAlpha;
+                    boardNode.setMinMaxMove(child.getParentMove());
+                }
+                if(beta <= alpha){
+                    break;
+                }
+            }
 
+            return tempAlpha;
+        }
+        else {
+            double tempBeta = Double.POSITIVE_INFINITY;
+            for(BoardNode child : boardNode.findChildren()){
+                if(child == null){
+                    break;
+                }
+                tempBeta = Math.min(tempBeta, MinMaxAlphaBeta(child, alpha, beta));
+                if (tempBeta < beta){
+                    beta = tempBeta;
+                    boardNode.setMinMaxMove(child.getParentMove());
+                }
+                if(beta <= alpha){
+                    break;
+                }
+            }
+            return beta;
+        }
     }
 }
